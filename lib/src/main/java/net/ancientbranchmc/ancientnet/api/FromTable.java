ccc1a5b96ecc8a4d9acd626b4c3286d19d5db9bd
SnowSnakz <snowsnakz@gmail.com>
2022-02-04 13:31:47 -0600

    Fixed `FromTable` to stay at runtime.

--- lib/src/main/java/net/ancientbranchmc/ancientnet/api/FromTable.java
+++ lib/src/main/java/net/ancientbranchmc/ancientnet/api/FromTable.java
@@ -1,5 +1,9 @@
 package net.ancientbranchmc.ancientnet.api;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface FromTable {
     public String database() default "";
     public String table() default "";
